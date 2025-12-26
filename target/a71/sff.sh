e891b4b445045a22af299acc5c6bba748a9dc398
artiyailbaraka <artiyailbaraka@gmail.com>
2025-12-26 19:26:29 +0700

    a71: ekstra feature

--- target/a71/sff.sh
+++ target/a71/sff.sh
@@ -26,3 +26,12 @@ SEC_FLOATING_FEATURE_CAMERA_CONFIG_VENDOR_LIB_INFO=image_codec.samsung.v1,beauty
 
 # Samsung Gallery app
 SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V448
+
+# Enable AOD live clock
+SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# Enable Video brightness feature
+SEC_FLOATING_FEATURE_COMMON_SUPPORT_HDR_EFFECT=true
