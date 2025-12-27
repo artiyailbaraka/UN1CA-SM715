5606d7adf06ad0defcc38a2bc8f4336366590f44
artiyailbaraka <artiyailbaraka@gmail.com>
2025-12-27 18:36:07 +0700

    display: rename memtrack

--- target/a71/patches/display/customize.sh
+++ target/a71/patches/display/customize.sh
@@ -61,6 +61,7 @@ ADD_TO_WORK_DIR "r8qxxx" "vendor" "lib64/hw/lights.kona.so" 0 0 644 "u:object_r:
 ADD_TO_WORK_DIR "r8qxxx" "vendor" "lib64/hw/memtrack.kona.so" 0 0 644 "u:object_r:vendor_configs_file:s0"
 
 mv "$WORK_DIR/vendor/lib64/hw/lights.kona.so" "$WORK_DIR/vendor/lib64/hw/lights.sm6150.so"
+mv "$WORK_DIR/vendor/lib64/hw/memtrack.kona.so" "$WORK_DIR/vendor/lib64/hw/memtrack.sm6150.so"
 
 HEX_PATCH "$WORK_DIR/vendor/lib64/libsdmutils.so" "40F9F303012A3401" "40F9130080523401"
 
