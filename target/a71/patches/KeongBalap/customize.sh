40dbf8b3b92fc9904e40a7471411b23c7b449cfd
artiyailbaraka <artiyailbaraka@gmail.com>
2026-01-03 19:38:23 +0700

    a71: use old kernel

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260103-095811"
+KERNEL_REPO="https://github.com/artiyailbaraka/Build_KeongBalap/releases/download/20251227-060135"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
