424d86819e5f2e732a1e8399bce507d7328057a8
artiyailbaraka <artiyailbaraka@gmail.com>
2026-01-04 08:56:55 +0700

    a71: kernel

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/artiyailbaraka/Build_KeongBalap/releases/download/20251227-060135"
+KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260104-012847"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
