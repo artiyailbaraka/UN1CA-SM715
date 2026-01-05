1a11bcc1e9512e08c019082edb87c2821c94f925
artiyailbaraka <artiyailbaraka@gmail.com>
2026-01-05 13:11:04 +0700

    a71: patches: upstream kernel 359

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260104-143357"
+KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260105-044056"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
