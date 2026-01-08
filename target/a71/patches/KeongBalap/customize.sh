090b7d7b82d52cf87c41ce3503a3f954673519c1
artiyailbaraka <artiyailbaraka@gmail.com>
2026-01-08 08:03:21 +0700

    a71: patches: kernel back to 365

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260107-152331"
+KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260107-144644"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
