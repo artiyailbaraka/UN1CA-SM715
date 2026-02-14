94a79aa31df39b3fb6c822e63b7f4e75d75f5fee
artiyailbaraka <artiyailbaraka@gmail.com>
2026-02-14 19:47:15 +0700

    a71: patches: kernel update

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260212-155510"
+KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260214-185914"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
