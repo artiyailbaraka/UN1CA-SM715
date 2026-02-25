b2d7301296badf3e74de9c0f06dc53fe6fe3d86d
artiyailbaraka <artiyailbaraka@gmail.com>
2026-02-25 09:41:13 +0700

    a71: patches: Kernel use latest tag

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,4 +1,4 @@
-KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/download/20260223-112722"
+KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/latest/download"
 
 LOG "- Removing old kernel images"
 [ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
