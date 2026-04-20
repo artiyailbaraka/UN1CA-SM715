b692f0a91a87d4d813135b8de6600389efeaa0fc
artiyailbaraka <artiyailbaraka@gmail.com>
2026-04-20 10:22:49 +0700

    a71: kernel change

--- target/a71/patches/KeongBalap/customize.sh
+++ target/a71/patches/KeongBalap/customize.sh
@@ -1,14 +1,8 @@
 KERNEL_REPO="https://github.com/SAM-715-ID/Bekicot_build/releases/latest/download"
 
-LOG "- Downloading kernel images"
-if [ -f "$WORK_DIR/kernel/boot.img" ]; then
-    EVAL "rm -f \"$WORK_DIR/kernel/boot.img\""
-fi
-if [ -f "$WORK_DIR/kernel/dtbo.img" ]; then
-    EVAL "rm -f \"$WORK_DIR/kernel/dtbo.img\""
-f
+LOG "- Removing old kernel images"
+[ -f "$WORK_DIR/kernel/boot.img" ] && rm -f "$WORK_DIR/kernel/boot.img"
+[ -f "$WORK_DIR/kernel/dtbo.img" ] && rm -f "$WORK_DIR/kernel/dtbo.img"
 
 DOWNLOAD_FILE "$KERNEL_REPO/boot.img" "$WORK_DIR/kernel/boot.img"
 DOWNLOAD_FILE "$KERNEL_REPO/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
-
-unset KERNEL_REPO
