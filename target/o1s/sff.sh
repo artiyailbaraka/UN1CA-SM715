d5b694930f07ad1859ddcb11759be52b54d15de9
nhglong <its.nhglong@gmail.com>
2026-05-09 21:39:21 +0700

    o1s/t2s/palette: floating_feature: restore stock LED cover level
    Change-Id: Id85ce964e9cf512b9b8e1cc90740250133a4284b

--- target/o1s/sff.sh
+++ target/o1s/sff.sh
@@ -6,8 +6,5 @@
 # Enable AOD live clock
 SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_AOD_ITEM=activeclock=7,aodversion=7,clocktransition,coverboldfont
 
-# Rainbow LED Cover level
-SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_NFC_LED_COVER_LEVEL=100
-
 # Enable extra brightness feature
 SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
