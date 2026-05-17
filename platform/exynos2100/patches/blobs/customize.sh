eaea71a8218e668af791cb6e641e89e6f614aa02
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-05-17 11:11:59 +0200

    exynos2100: patches: blobs: add stock LedBackCoverAppUnbound apk
    Change-Id: I9a8eed7bddeba70df0062c37149475d958134e0e

--- platform/exynos2100/patches/blobs/customize.sh
+++ platform/exynos2100/patches/blobs/customize.sh
@@ -34,6 +34,11 @@ fi
 
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/android.hardware.sensor.hifi_sensors.xml" 0 0 644 "u:object_r:system_file:s0"
 
+if [[ "$TARGET_CODENAME" != "r9s" ]]; then
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.app.ledbackcover.xml" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/priv-app/LedBackCoverAppUnbound/LedBackCoverAppUnbound.apk" 0 0 644 "u:object_r:system_file:s0"
+fi
+
 LOG_STEP_IN "- Adding 32-bit WFD blobs"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/bin/insthk" 0 2000 755 "u:object_r:insthk_exec:s0"
 ADD_TO_WORK_DIR "r9sxxx" "system" "system/bin/remotedisplay" 0 2000 755 "u:object_r:remotedisplay_exec:s0"
